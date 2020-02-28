1751658e8d82dd9f40f6863b2070fdbf03f5211d
Andre Watts <andrewattsii@gmail.com>
2020-02-28 14:45:31 -0500

    thinking about what funcions i need to add beforehand, also switchedup the background. looks a lot  neater

--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -2,11 +2,32 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+    }
+    fun startQuiz(){
+
+    }
+
+
+    fun showAnswer(view:View) {
+
+    }
+    fun checkAnswer(view: View){
+
+    }
+    fun changeQuestion(view :View){
+
+    }
+    fun rightAnswer(){
+
+    }
+    fun wrongAnswer(){
+        
     }
 }
