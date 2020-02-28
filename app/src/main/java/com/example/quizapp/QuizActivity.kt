ecbd7fbd0fa757f0ce70063425c9d2ee46f63e19
Andre Watts <andrewattsii@gmail.com>
2020-02-28 15:15:23 -0500

    created views making them available to be used and crateds a new instance of questins...aaand yes that is a array of string housing questions

--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -3,12 +3,22 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class QuizActivity : AppCompatActivity() {
+
+    var questions = arrayOf("Is the Amazon the biggest rain forest in the word?", "Roughly how big is the amazon?", "Does teh Amazon have the largest river?", "What mountain range influences the flow of the amazon river?", "In what country is most of the Amazons forest located in?")
+    lateinit var right_answer: TextView
+    lateinit var question_textView: TextView
+    lateinit var user_answer: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        right_answer = findViewById(R.id.right_answer)
+        question_textView = findViewById(R.id.question_textview)
+        user_answer = findViewById(R.id.user_answer)
+
     }
     fun startQuiz(){
 
@@ -28,6 +38,6 @@ class QuizActivity : AppCompatActivity() {
 
     }
     fun wrongAnswer(){
-        
+
     }
 }
